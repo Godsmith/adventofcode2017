1c3554deb6c4c1f2ed21f80a7cece27256f8ee27
Filip Lange <filip.lange@gmail.com>
2017-12-05 11:31:49 +0100

    Cleanup

--- day1.py
+++ day1.py
@@ -3,17 +3,28 @@ from util import input_rows
 
 def main():
     print(solve_captcha(input_rows(1)[0]))
+    print(solve_captcha_part_two(input_rows(1)[0]))
 
 
 def solve_captcha(input_):
-    digits = [int(current) for current, next_ in current_and_next(input_) if
-              current == next_]
+    return calculate_sum(input_, 1)
+
+
+def solve_captcha_part_two(input_):
+    offset = int(len(input_) / 2)
+    return calculate_sum(input_, offset)
+
+
+def calculate_sum(input_, offset):
+    digits = [int(current) for current, halfway in current_and_offset(
+        input_, offset) if current == halfway]
     return sum(digits)
 
 
-def current_and_next(collection):
+def current_and_offset(collection, offset):
+    length = len(collection)
     for i, element in enumerate(collection):
-        yield (element, collection[(i + 1) % len(collection)])
+        yield (element, collection[(i + offset) % length])
 
 
 if __name__ == '__main__':
--- test_day1.py
+++ test_day1.py
@@ -1,8 +1,14 @@
-from day1 import current_and_next, solve_captcha
+from day1 import solve_captcha, solve_captcha_part_two, current_and_offset
 
 
-def test_current_and_next():
-    assert list(current_and_next('123')) == [("1", "2"), ("2", "3"), ("3", "1")]
+def test_current_and_offset():
+    assert list(current_and_offset('123', 1)) == [("1", "2"),
+                                                  ("2", "3"),
+                                                  ("3", "1")]
+    assert list(current_and_offset('1234', 2)) == [("1", "3"),
+                                                   ("2", "4"),
+                                                   ("3", "1"),
+                                                   ("4", "2")]
 
 
 def test_solve_captcha():
@@ -10,3 +16,11 @@ def test_solve_captcha():
     assert solve_captcha('1111') == 4
     assert solve_captcha('1234') == 0
     assert solve_captcha('91212129') == 9
+
+
+def test_solve_captcha_part_two():
+    assert solve_captcha_part_two('1212') == 6
+    assert solve_captcha_part_two('1221') == 0
+    assert solve_captcha_part_two('123425') == 4
+    assert solve_captcha_part_two('123123') == 12
+    assert solve_captcha_part_two('12131415') == 4
