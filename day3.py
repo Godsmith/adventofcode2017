d88f48000087a580691f3aa8c70bbd0c597591c4
Filip Lange <filip.lange@gmail.com>
2017-12-07 15:20:58 +0100

    refactoring

--- day3.py
+++ day3.py
@@ -47,70 +47,100 @@ class Step:
         return 'Step(%s)' % self.direction
 
 
-def main():
-    SIDE = 603  # Big enough
-    print(distance_to_center(SIDE, 361527))
-    square = AdjacencySpiralSquare(SIDE)
-
-
-def distance_to_center(side, n):
-    middle = int(side / 2 - 0.5)
-    table = SpiralSquare(side).table
-    for x, y in product(range(side), range(side)):
-        if table[x][y] == n:
-            return abs(x - middle) + abs(y - middle)
-
-
 class SpiralSquare:
     def __init__(self, side):
-        largest_x = largest_y = smallest_x = smallest_y = 0
-        self.values = {}
+        self._side = side
+        self._values = {}
+        self.largest_x = self.largest_y = self.smallest_x = self.smallest_y = 0
+
+    @property
+    def table(self):
         position = Position(0, 0)
         step = Step(Direction.RIGHT)
         index = 1
-        while largest_x - smallest_x < side:
-            self.values[(position.x, position.y)] = self.square_value(index,
-                                                                      position)
+        while self.largest_x - self.smallest_x < self._side:
+            self._values[(position.x, position.y)] = self._square_value(index,
+                                                                        position)
             position += step
             index += 1
-            if position.x > largest_x:
-                largest_x = position.x
+            if self._at_edge(position):
                 step.turn()
-            if position.y > largest_y:
-                largest_y = position.y
-                step.turn()
-            if position.x < smallest_x:
-                smallest_x = position.x
-                step.turn()
-            if position.y < smallest_y:
-                smallest_y = position.y
-                step.turn()
-        output = [[None] * side for _ in range(side)]
-        x_offset = -min(self.values.keys(), key=lambda p: p[0])[0]
-        y_offset = -min(self.values.keys(), key=lambda p: p[1])[1]
-        for x, y in self.values.keys():
+        return self._create_table()
+
+    def _at_edge(self, position):
+        old_edges = self._edges
+        self.largest_x = max(self.largest_x, position.x)
+        self.largest_y = max(self.largest_y, position.y)
+        self.smallest_x = min(self.smallest_x, position.x)
+        self.smallest_y = min(self.smallest_y, position.y)
+        return self._edges != old_edges
+
+    @property
+    def _edges(self):
+        return (self.largest_x, self.largest_y,
+                self.smallest_x, self.smallest_y)
+
+    def _create_table(self):
+        output = [[None] * self._side for _ in range(self._side)]
+        x_offset = -min(self._values.keys(), key=lambda p: p[0])[0]
+        y_offset = -min(self._values.keys(), key=lambda p: p[1])[1]
+        for x, y in self._values.keys():
             new_x = x_offset + x
             new_y = y_offset + y
-            output[new_y][new_x] = self.values[(x, y)]
-        self.table = output
+            output[new_y][new_x] = self._values[(x, y)]
+        return output
 
-    def square_value(self, index, position):
+    def _square_value(self, index, position):
+        raise NotImplementedError
+
+
+class IndexSpiralSquare(SpiralSquare):
+    def _square_value(self, index, position):
         return index
 
 
 class AdjacencySpiralSquare(SpiralSquare):
-    def square_value(self, index, position):
+    def __init__(self, side, break_value):
+        super(AdjacencySpiralSquare, self).__init__(side)
+        self._break_value = break_value
+
+    def _square_value(self, index, position):
         if position.x == position.y == 0:
             return 1
-        x_values = range(position.x - 1, position.x + 2)
-        y_values = range(position.y - 1, position.y + 2)
-        sum_ = sum(self.values.get((x, y), 0) for x, y in
-                   product(x_values, y_values))
-        if sum_ > 361527:
-            print(sum_)
-            exit()
+        sum_ = self._sum_of_adjacent_squares(position)
+        if sum_ > self._break_value:
+            raise FinishedException(sum_)
         else:
             return sum_
+
+    def _sum_of_adjacent_squares(self, position):
+        x_values = range(position.x - 1, position.x + 2)
+        y_values = range(position.y - 1, position.y + 2)
+        return sum(self._values.get((x, y), 0) for x, y in
+                   product(x_values, y_values))
+
+
+class FinishedException(Exception):
+    pass
+
+
+def main():
+    SIDE = 603  # Big enough
+    VALUE = 361527
+    print(distance_to_center(SIDE, VALUE))
+    square = AdjacencySpiralSquare(SIDE, VALUE)
+    try:
+        a = square.table
+    except FinishedException as e:
+        print(e.args[0])
+
+
+def distance_to_center(side, n):
+    middle = int(side / 2 - 0.5)
+    table = IndexSpiralSquare(side).table
+    for x, y in product(range(side), range(side)):
+        if table[x][y] == n:
+            return abs(x - middle) + abs(y - middle)
 
 
 if __name__ == '__main__':
