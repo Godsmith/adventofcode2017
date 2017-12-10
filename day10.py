8165d4433f08b12cf5ce68041e342b6f1d8820ec
Filip Lange <filip.lange@gmail.com>
2017-12-10 21:57:32 +0100

    After refactoring to class

--- day10.py
+++ day10.py
@@ -9,60 +9,64 @@ def parse_input_for_part_two(input_string):
     return list(map(ord, input_string)) + [17, 31, 73, 47, 23]
 
 
-def tie_knot(list_, current_position, input_, skip):
-    new_list = reverse_part(list_, current_position, input_)
-    return new_list, (current_position + input_ + skip) % len(list_), skip + 1
+class KnotHash:
+    def __init__(self, inputs=None):
+        self.inputs = inputs
 
+    @classmethod
+    def tie_knot(cls, list_, current_position, input, skip):
+        new_list = cls._reverse_part(list_, current_position, input)
+        return new_list, (current_position + input + skip) % len(
+            list_), skip + 1
 
-def reverse_part(list_, start, length):
-    old_indices = [i % len(list_) for i in range(start, start + length)]
-    new_indices = reversed(old_indices)
-    new_list = list(list_)
-    for old, new in zip(old_indices, new_indices):
-        new_list[new] = list_[old]
-    return new_list
+    @classmethod
+    def _reverse_part(cls, list_, start, length):
+        old_indices = [i % len(list_) for i in range(start, start + length)]
+        new_indices = reversed(old_indices)
+        new_list = list(list_)
+        for old, new in zip(old_indices, new_indices):
+            new_list[new] = list_[old]
+        return new_list
 
+    def knot_hash(self, list_, position=0, skip=0):
+        for input_ in self.inputs:
+            list_, position, skip = self.tie_knot(list_, position, input_, skip)
+        return list_, position, skip
 
-def knot_hash(list_, inputs, position=0, skip=0):
-    for input_ in inputs:
-        list_, position, skip = tie_knot(list_, position, input_, skip)
-    return list_, position, skip
+    def sparse_hash(self, list_):
+        position = 0
+        skip = 0
+        for _ in range(64):
+            list_, position, skip = self.knot_hash(list_, position, skip)
+        return list_
 
+    @classmethod
+    def dense_hash(cls, sparse_hash_):
+        return [reduce(xor, sparse_hash_[i:i + 16])
+                for i in range(0, len(sparse_hash_), 16)]
 
-def sparse_hash(list_, inputs):
-    position = 0
-    skip = 0
-    for _ in range(64):
-        list_, position, skip = knot_hash(list_, inputs, position, skip)
-    return list_
+    @classmethod
+    def dense_hash_string(cls, dense_hash_list):
+        return ''.join(map(cls.my_hex, dense_hash_list))
 
+    @classmethod
+    def my_hex(cls, int_):
+        s = hex(int_)[2:]
+        if len(s) == 1:
+            return '0' + s
+        return s
 
-def dense_hash(sparse_hash_):
-    return [reduce(xor, sparse_hash_[i:i + 16])
-            for i in range(0, len(sparse_hash_), 16)]
-
-
-def dense_hash_string(dense_hash_list):
-    return ''.join(map(my_hex, dense_hash_list))
-
-
-def my_hex(int_):
-    s = hex(int_)[2:]
-    if len(s) == 1:
-        return '0' + s
-    return s
-
-
-def knot_hash_string(input_):
-    input_list = parse_input_for_part_two(input_)
-    sparse_hash_ = sparse_hash(list(range(256)), input_list)
-    dense_hash_ = dense_hash(sparse_hash_)
-    return dense_hash_string(dense_hash_)
+    def knot_hash_string(self):
+        self.inputs = parse_input_for_part_two(self.inputs)
+        sparse_hash_ = self.sparse_hash(list(range(256)))
+        dense_hash_ = self.dense_hash(sparse_hash_)
+        return self.dense_hash_string(dense_hash_)
 
 
 if __name__ == '__main__':
     list_ = list(range(256))
-    output_list, _, _ = knot_hash(list_, INPUT_PART_1)
+    knot_hash = KnotHash(INPUT_PART_1)
+    output_list, _, _ = knot_hash.knot_hash(list_)
     print(output_list[0] * output_list[1])
 
-    print(knot_hash_string(INPUT_STRING))
+    print(knot_hash.knot_hash_string())
--- test_day10.py
+++ test_day10.py
@@ -1,12 +1,11 @@
 import pytest
 
-from day10 import tie_knot, knot_hash, reverse_part, \
-    parse_input_for_part_two, dense_hash, my_hex, dense_hash_string, \
-    knot_hash_string
+from day10 import KnotHash, parse_input_for_part_two
 
 
 def test_reverse_part():
-    assert reverse_part([2, 1, 0, 3, 4], start=3, length=4) == [4, 3, 0, 1, 2]
+    assert KnotHash()._reverse_part([2, 1, 0, 3, 4], start=3, length=4) == \
+           [4, 3, 0, 1, 2]
 
 
 @pytest.mark.parametrize('list_, current_position, input_, skip, '
@@ -17,10 +16,10 @@ def test_reverse_part():
                            [4, 3, 0, 1, 2], 3, 2)])
 def test_tie_knot(list_, current_position, input_, skip, new_list,
                   new_position, new_skip):
-    assert tie_knot(
+    assert KnotHash(input_).tie_knot(
         list_=list_,
         current_position=current_position,
-        input_=input_,
+        input=input_,
         skip=skip) == (new_list, new_position, new_skip)
 
 
@@ -28,30 +27,32 @@ def test_tie_knot(list_, current_position, input_, skip, new_list,
                          [([0, 1, 2, 3, 4], [3], [2, 1, 0, 3, 4]),
                           ([0, 1, 2, 3, 4], [3, 4], [4, 3, 0, 1, 2]),
                           ([0, 1, 2, 3, 4], [3, 4, 1, 5], [3, 4, 2, 1, 0])])
-def test_tie_knots(list_, inputs, output):
-    knot_hash_list, _, _ = knot_hash(list_, inputs)
+def test_knot_hash(list_, inputs, output):
+    knot_hash_list, _, _ = KnotHash(inputs).knot_hash(list_)
     assert knot_hash_list == output
 
 
 def test_parse_input_for_part_two():
-    assert parse_input_for_part_two('1,2,3') == [49, 44, 50, 44, 51, 17, 31, 73,
+    assert parse_input_for_part_two('1,2,3') == [49, 44, 50, 44, 51,
+                                                 17, 31, 73,
                                                  47, 23]
 
 
 def test_dense_hash():
     hash = [65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22]
-    assert dense_hash(hash) == [64]
-    assert len(dense_hash(range(256))) == 16
+    assert KnotHash.dense_hash(hash) == [64]
+    assert len(KnotHash.dense_hash(range(256))) == 16
 
 
 def test_dense_hash_string():
-    assert dense_hash_string([64, 7, 255]) == '4007ff'
+    assert KnotHash.dense_hash_string([64, 7, 255]) == '4007ff'
 
 
 def test_my_hex():
-    assert my_hex(64) == '40'
+    assert KnotHash.my_hex(64) == '40'
 
 
 def test_knot_hash_string():
-    assert knot_hash_string('AoC 2017') == '33efeb34ea91902bb2f59c9920caa6cd'
-    assert knot_hash_string('') == 'a2582a3a0e66e6e86e3812dcb672a272'
+    assert KnotHash('AoC 2017').knot_hash_string() == \
+           '33efeb34ea91902bb2f59c9920caa6cd'
+    assert KnotHash('').knot_hash_string() == 'a2582a3a0e66e6e86e3812dcb672a272'
