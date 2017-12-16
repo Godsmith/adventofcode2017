417492f73736959d5b80f25d523c2b8b8f7a6cca
Filip Lange <filip.lange@gmail.com>
2017-12-16 22:32:05 +0100

    New util function input_list

--- util.py
+++ util.py
@@ -15,10 +15,18 @@ def input_lists(day_nr, delimiter='\t'):
         return list(line.strip().split(delimiter) for line in f.readlines())
 
 
+def input_list(day_nr, delimiter=','):
+    return input_lists(day_nr, delimiter)[0]
+
+
 def test_input_rows():
     assert input_rows('test') == ['hello', 'world']
 
 
 def test_input_lists():
-    assert input_lists('test_list') == [['hello', 'world', 'again'],
-                                       ['how', 'are', 'you']]
+    assert input_lists('test_lists') == [['hello', 'world', 'again'],
+                                         ['how', 'are', 'you']]
+
+
+def test_input_list():
+    assert input_list('test_list') == ['1', '2', '3', '4', '5']
