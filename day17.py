670f7940acf70f0d84ad5ff26f1ed962757d97e7
Filip Lange <filip.lange@gmail.com>
2017-12-19 10:35:11 +0100

    Optimization

--- day17.py
+++ day17.py
@@ -17,9 +17,26 @@ def spinlock_value_after_value(steps, iterations, value):
     lock = spinlock(steps)
     for _ in range(iterations):
         list_ = next(lock)
-    return list_[list_.index(2017) + 1]
+    return list_[list_.index(value) + 1]
+
+
+def spinlock_value_after_0(steps, iterations):
+    """More optimized version of this special case"""
+    position_of_0 = 0
+    position = 0
+    length = 1
+    value_after_0 = None
+    for _ in range(iterations):
+        position = (position + steps) % length
+        if position < position_of_0:
+            position_of_0 += 1
+        elif position == position_of_0:
+            value_after_0 = length
+        position += 1
+        length += 1
+    return value_after_0
 
 
 if __name__ == '__main__':
     print(spinlock_value_after_value(303, 2017, 2017))
-    print(spinlock_value_after_value(303, 50000000, 0))
+    print(spinlock_value_after_0(303, 50000000))
--- test_day17.py
+++ test_day17.py
@@ -1,6 +1,6 @@
 from blist import blist
 
-from day17 import spinlock
+from day17 import spinlock, spinlock_value_after_value, spinlock_value_after_0
 
 
 def test_spinlock():
@@ -9,3 +9,12 @@ def test_spinlock():
     assert next(lock) == blist([0, 2, 1])
     assert next(lock) == blist([0, 2, 3, 1])
     assert next(lock) == blist([0, 2, 4, 3, 1])
+
+
+def test_spinlock_value_after_value():
+    assert spinlock_value_after_value(303, 2017, 2017) == 1971
+
+
+def test_spinlock_value_after_0():
+    assert (spinlock_value_after_0(303, 2017) ==
+            spinlock_value_after_value(303, 2017, 0))
