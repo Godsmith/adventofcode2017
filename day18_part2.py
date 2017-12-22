965b05e333ac0a2d8a26940e5d87c623f22e2eae
Filip Lange <filip.lange@gmail.com>
2017-12-22 22:36:49 +0100

    part2 working tests but not for real

---
+++ day18_part2.py
@@ -0,0 +1,67 @@
+from collections import defaultdict, deque
+
+from util import input_rows
+
+
+class Program:
+    def __init__(self, id):
+        self._registers = defaultdict(lambda: 0)
+        self._registers['p'] = id
+        self._index = 0
+        self._message_queue = deque()
+        self._peer_program = None
+        self.send_count = 0
+        self.waiting = False
+
+    def peer(self, program):
+        self._peer_program = program
+        program._peer_program = self
+
+    def send(self, value):
+        self.send_count += 1
+        self._message_queue.appendleft(value)
+
+    def follow_instruction(self, string):
+        instruction, *params = string.split()
+        if instruction == 'snd':
+            self._peer_program.send(self._to_value(params[0]))
+        elif instruction == 'set':
+            self._registers[params[0]] = self._to_value(params[1])
+        elif instruction == 'add':
+            self._registers[params[0]] += self._to_value(params[1])
+        elif instruction == 'mul':
+            self._registers[params[0]] *= self._to_value(params[1])
+        elif instruction == 'mod':
+            self._registers[params[0]] %= self._to_value(params[1])
+        elif instruction == 'rcv':
+            if len(self._message_queue) == 0:
+                self.waiting = True
+                return
+            self._registers[params[0]] = self._message_queue.pop()
+            self.waiting = False
+        elif instruction == 'jgz':
+            if self._to_value(params[0]) > 0:
+                self._index += self._to_value(params[1])
+                return
+        self._index += 1
+
+    def _to_value(self, value_or_register):
+        try:
+            return int(value_or_register)
+        except ValueError:
+            return self._registers[value_or_register]
+
+
+def send_count_of_second_program_at_deadlock(programs, instructions):
+    programs[0].peer(programs[1])
+    while any([not p.waiting for p in programs]):
+        for instruction in instructions:
+            for p in programs:
+                p.follow_instruction(instruction)
+    return programs[1].send_count
+
+
+if __name__ == '__main__':
+    p0 = Program(0)
+    p1 = Program(1)
+    print(send_count_of_second_program_at_deadlock([p0, p1], input_rows(18)))
---
+++ test_day18_part2.py
@@ -0,0 +1,17 @@
+from day18_part2 import Program, send_count_of_second_program_at_deadlock
+
+
+class TestProgram:
+    def test_init(self):
+        p = Program(1)
+        assert p._registers['p'] == 1
+
+
+class TestSendCountOfSecondProgramAtDeadlock:
+    def test_example(self):
+        instructions = ['snd 1', 'snd 2', 'snd p', 'rcv a',
+                        'rcv b', 'rcv c', 'rcv d']
+        p0 = Program(0)
+        p1 = Program(1)
+        assert send_count_of_second_program_at_deadlock([p0, p1],
+                                                        instructions) == 3
