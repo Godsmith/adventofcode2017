c50a26c6b8a17a40fac93e01549f4a62e509376f
Filip Lange <filip.lange@gmail.com>
2017-12-25 22:21:18 +0100

    Finally finished

--- day23_part2.py
+++ day23_part2.py
@@ -6,14 +6,14 @@ while True:
     f = 1
     d = 2
     while g != 0:
-        f = 0
-        e = b
+        if b % d == 0:
+            f = 0
         d += 1
         g = d - b
-        print(g)
     if f == 0:
         h += 1
     g = b - c
+    print(g)
     if g == 0:
         print(h)
         exit()
