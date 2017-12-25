57f7d343f8a96cbcaf3053027d79fb404cc6c3c8
Filip Lange <filip.lange@gmail.com>
2017-12-25 22:02:38 +0100

    removed inner loop

--- day23_part2.py
+++ day23_part2.py
@@ -6,14 +6,8 @@ while True:
     f = 1
     d = 2
     while g != 0:
-        e = 2
-        g = d
-        while g != 0:
-            g = d * e - b
-            if g == 0:
-                f = 0
-            e += 1
-            g = e - b
+        f = 0
+        e = b
         d += 1
         g = d - b
         print(g)
