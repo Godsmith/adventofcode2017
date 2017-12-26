74903f8bcef4254cd6b44a798c92cd973fa1ff52
Godsmith <filip.lange@gmail.com>
2017-12-26 12:45:42 +0100

    day20 part1

--- day20.py
+++ day20.py
@@ -1,16 +1,15 @@
 from util import input_rows
 
+
 class Particle:
     def __init__(self, position, velocity, acceleration):
         self.position = position
         self.velocity = velocity
         self.acceleration = acceleration
-        self._distance = self._distance_from_origin(self.position)
-        self._distance_last = self._distance
-        self._distance_before_last = self._distance
 
     def __repr__(self):
-        return f'Particle({self.position}, {self.velocity}, {self.acceleration})'
+        return f
+        'Particle({self.position}, {self.velocity}, {self.acceleration})'
 
     @classmethod
     def from_string(cls, s):
@@ -22,26 +21,33 @@ class Particle:
         return cls(*args)
 
     def tick(self):
-        self._distance_before_last = self._distance_last
-        self._distance_last = self._distance
         for i, _ in enumerate(self.position):
             self.velocity[i] += self.acceleration[i]
             self.position[i] += self.velocity[i]
-        self._distance = self._distance_from_origin(self.position)
 
     @property
     def distance_from_origin(self):
-        return self._distance
-
-    @staticmethod
-    def _distance_from_origin(position):
-        return sum(map(abs, position))
+        return sum(map(abs, self.position))
 
     @property
-    def moving_away_from_origin(self):
-        most_recent_distance_delta = self._distance - self._distance_last
-        previous_distance_delta = self._distance_last - self._distance_before_last
-        return most_recent_distance_delta >= previous_distance_delta
+    def total_acceleration(self):
+        return sum(map(abs, self.acceleration))
+
+    @property
+    def is_moving_away_from_origin(self):
+        for i, coordinate in enumerate(self.position):
+            if not self._has_same_sign(self.position[i], self.velocity[i], self.acceleration[i]):
+                return False
+        return True
+
+    @staticmethod
+    def _has_same_sign(*args):
+        if all(map(lambda x: x >= 0, args)):
+            return True
+        if all(map(lambda x: x <= 0, args)):
+            return True
+        return False
+
 
 class ParticleSwarm:
     def __init__(self, particles):
@@ -52,29 +58,36 @@ class ParticleSwarm:
         particles = list(map(Particle.from_string, list_))
         return cls(particles)
 
-    @property
-    def index_closest_to_origin(self):
-        distances = [p.distance_from_origin for p in self._particles]
-        return distances.index(min(distances))
-
     def index_closest_to_origin_long_term(self):
-        self.tick()
-        self.tick()
-        while not self.all_moving_away_from_origin:
-            print('tick')
-            self.tick()
-        return self.index_closest_to_origin
+        return self._particles.index(self._particle_closest_to_origin_long_term())
+
+    def _particle_closest_to_origin_long_term(self):
+        while not self._all_moving_away_from_origin:
+            self._tick()
+        return list(self._particles_with_minimum_distance_to_origin(self._particles_with_minimum_acceleration()))[0]
 
     @property
-    def all_moving_away_from_origin(self):
-        return all((p.moving_away_from_origin for p in self._particles))
+    def _all_moving_away_from_origin(self):
+        return all((p.is_moving_away_from_origin for p in self._particles))
 
-    def tick(self):
+    def _tick(self):
         for particle in self._particles:
             particle.tick()
+
+    def _minimum_acceleration(self):
+        return min(p.total_acceleration for p in self._particles)
+
+    def _particles_with_minimum_acceleration(self):
+        return set(p for p in self._particles if p.total_acceleration == self._minimum_acceleration())
+
+    @staticmethod
+    def _minimum_distance_to_origin(particles):
+        return min(p.distance_from_origin for p in particles)
+
+    def _particles_with_minimum_distance_to_origin(self, particles):
+        return set(p for p in self._particles if p.distance_from_origin == self._minimum_distance_to_origin(particles))
 
 
 if __name__ == '__main__':
     swarm = ParticleSwarm.from_strings(input_rows(20))
     print(swarm.index_closest_to_origin_long_term())
-
--- test_day20.py
+++ test_day20.py
@@ -19,23 +19,21 @@ class TestParticle:
         particle = Particle.from_string('p=< 3,2,-7>, v=< 2,0,0>, a=<-1,0,0>')
         assert particle.distance_from_origin == 12
 
+    def test_acceleration_from_origin(self):
+        particle = Particle.from_string('p=< 3,2,-7>, v=< 2,0,0>, a=<-1,2,0>')
+        assert particle.total_acceleration == 3
+
+    def test_has_same_sign(self):
+        assert Particle._has_same_sign(1, 0, 2)
+        assert Particle._has_same_sign(-1, 0, -2)
+        assert not Particle._has_same_sign(1, 0, -2)
+
     def test_moving_away_from_origin(self):
         particle = Particle.from_string('p=< 4,0,0>, v=<-1,0,0>, a=<0,0,0>')
-        particle.tick()
-        assert not particle.moving_away_from_origin
-
+        assert not particle.is_moving_away_from_origin
 
         particle = Particle.from_string('p=< 4,0,0>, v=<1,0,0>, a=<0,0,0>')
-        particle.tick()
-        particle.tick()
-        print(particle._distance_last)
-        print(particle._distance_before_last)
-        assert particle.moving_away_from_origin
-
-        particle = Particle.from_string('p=< 4,0,0>, v=<1,0,0>, a=<-1,0,0>')
-        particle.tick()
-        particle.tick()
-        assert not particle.moving_away_from_origin
+        assert particle.is_moving_away_from_origin
 
 
 class TestParticleSwarm:
@@ -43,21 +41,16 @@ class TestParticleSwarm:
         swarm = ParticleSwarm.from_strings(['p=< 3,2,-7>, v=< 2,0,0>, a=<-1,0,0>'])
         assert swarm._particles[0].velocity == [2, 0, 0]
 
-    def test_index_closest_to_origin(self):
-        swarm = ParticleSwarm.from_strings(['p=< 3,2,-7>, v=< 2,0,0>, a=<-1,0,0>',
-                                            'p=< 2,2,-7>, v=< 2,0,0>, a=<-1,0,0>'])
-        assert swarm.index_closest_to_origin == 1
-
     def test_all_moving_away_from_origin(self):
         swarm = ParticleSwarm.from_strings(['p=< 3,0,0>, v=< 2,0,0>, a=<-1,0,0>',
                                             'p=< 4,0,0>, v=< 0,0,0>, a=<-2,0,0>'])
-        swarm.tick()
-        swarm.tick()
-        assert not swarm.all_moving_away_from_origin
-        swarm.tick()
-        swarm.tick()
-        swarm.tick()
-        assert swarm.all_moving_away_from_origin
+        swarm._tick()
+        swarm._tick()
+        assert not swarm._all_moving_away_from_origin
+        swarm._tick()
+        swarm._tick()
+        swarm._tick()
+        assert swarm._all_moving_away_from_origin
 
     def test_index_closest_to_origin_long_term(self):
         swarm = ParticleSwarm.from_strings(['p=< 3,0,0>, v=< 2,0,0>, a=<-1,0,0>',
