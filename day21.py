787eb93c3a1334685ee7d55b6bb0053610a25617
Filip Lange <filip.lange@gmail.com>
2017-12-28 20:46:47 +0100

    day21, forgot flips

--- day21.py
+++ day21.py
@@ -1,6 +1,8 @@
 from itertools import product
 from math import sqrt
 
+from util import input_rows
+
 
 class Square:
     def __init__(self, rows):
@@ -10,6 +12,9 @@ class Square:
         return self.rows == other.rows
 
     def __repr__(self):
+        return '/'.join(self.rows)
+
+    def __str__(self):
         return '/'.join(self.rows)
 
     def __hash__(self):
@@ -24,10 +29,23 @@ class Square:
             self._size_of_child_squares(), row, column))
             for row, column in self._iterator_over_child_square_coordinates()]
 
-    def combine(self, squares):
-        size = int(sqrt(len(squares)))
+    @classmethod
+    def combine(cls, squares):
+        child_squares_per_row = int(sqrt(len(squares)))
+        child_square_size = len(squares[0].rows[0])
+        rows = []
+        for square_row in [squares[i:i + child_squares_per_row] for i in range(
+                0, len(squares), child_squares_per_row)]:
+            for row in range(child_square_size):
+                rows.append(''.join(square.rows[row] for square in square_row))
+        return cls(rows)
 
+    def _iterate(self, book):
+        return self.combine([square._enhance(book) for square in
+                             self.break_up()])
 
+    def _count_pixels_on(self):
+        return sum(row.count('#') for row in self.rows)
 
     def _square_at_position(self, size, row, column):
         return [self.rows[row + i][column:column + size] for i in range(size)]
@@ -41,3 +59,49 @@ class Square:
     def _iterate_with_step_size(self, size):
         return product(range(0, len(self.rows), size),
                        range(0, len(self.rows), size))
+
+    def _rotate(self):
+        return Square([''.join(column) for column in zip(*reversed(self.rows))])
+
+    def _rotations(self):
+        rotate1 = self._rotate()
+        rotate2 = rotate1._rotate()
+        rotate3 = rotate2._rotate()
+        return {self, rotate1, rotate2, rotate3}
+
+    def _enhance(self, book):
+        rotations = self._rotations()
+        for key in book:
+            if key in rotations:
+                return book[key]
+        raise ValueError(f'None of squares in {rotations} found in book!')
+
+
+class EnhancementBook:
+    def __init__(self, translations):
+        self._translations = translations
+
+    def __getitem__(self, item):
+        return self._translations[item]
+
+    def __iter__(self):
+        return iter(self._translations)
+
+    @classmethod
+    def from_strings(cls, strings):
+        dict_ = {}
+        for string in strings:
+            source, _, target = string.split()
+            source_square = Square.from_string(source)
+            target_square = Square.from_string(target)
+            for rotated_source_square in source_square._rotations():
+                dict_[rotated_source_square] = target_square
+        return EnhancementBook(dict_)
+
+
+if __name__ == '__main__':
+    book = EnhancementBook.from_strings(input_rows(21))
+    square = Square.from_string('.#./..#/###')
+    for _ in range(5):
+        square = square._iterate(book)
+    print(square._count_pixels_on())
--- test_day21.py
+++ test_day21.py
@@ -1,4 +1,6 @@
-from day21 import Square
+from day21 import Square, EnhancementBook
+
+from util import input_rows
 
 
 class TestSquare:
@@ -19,3 +21,56 @@ class TestSquare:
                                Square.from_string('##/##'),
                                Square.from_string('#./..'),
                                Square.from_string('../..')])
+
+    def test_rotate(self):
+        square = Square.from_string('###/###/...')
+        assert square._rotate() == Square.from_string('.##/.##/.##')
+        assert square._rotate()._rotate() == Square.from_string('.../###/###')
+
+    def test_rotations(self):
+        square = Square.from_string('###/###/...')
+        assert square._rotations() == {square,
+                                       Square.from_string('.##/.##/.##'),
+                                       Square.from_string('.../###/###'),
+                                       Square.from_string('##./##./##.')}
+
+    def test_rotations_harder(self):
+        square = Square.from_string('.#./..#/###')
+        assert square._rotations() == {square,
+                                       Square.from_string('.#./#../###'),
+                                       Square.from_string('.../###/###'),
+                                       Square.from_string('##./##./##.')}
+
+    def test_enhance(self):
+        square = Square.from_string('##/..')
+        book = {Square.from_string('##/..'): Square.from_string('###/.../###')}
+        assert square._enhance(book) == Square.from_string('###/.../###')
+
+    def test_enhance_rotated(self):
+        square = Square.from_string('##/..')
+        book = {Square.from_string('#./#.'): Square.from_string('###/.../###')}
+        assert square._enhance(book) == Square.from_string('###/.../###')
+
+    def test_count_pixels_on(self):
+        square = Square.from_string('##/#.')
+        assert square._count_pixels_on() == 3
+
+
+class TestEnhancementBook:
+    def test_translate(self):
+        book = EnhancementBook.from_strings(['../.. => ###/.##/#..',
+                                             '#./.. => #.#/..#/#..'])
+        assert book[
+                   Square.from_string('#./..')] == Square.from_string(
+            '#.#/..#/#..')
+
+    def test_translate_rotated(self):
+        book = EnhancementBook.from_strings(['../.. => ###/.##/#..',
+                                             '#./.. => #.#/..#/#..'])
+        assert book[
+                   Square.from_string('../.#')] == Square.from_string(
+            '#.#/..#/#..')
+
+    def test_missing_square(self):
+        book = EnhancementBook.from_strings(input_rows(21))
+        assert Square.from_string('###/###/###') in book
