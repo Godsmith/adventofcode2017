f61d2de4a8311b76b1a5b66aed251e9d2fa09c6e
Filip Lange <filip.lange@gmail.com>
2017-12-29 23:12:56 +0100

    day22 part2

--- day22.py
+++ day22.py
@@ -39,7 +39,7 @@ class Virus:
         self._grid.clean(self._position)
 
     def _is_current_node_infected(self):
-        return self._grid.infected(self._position)
+        return self._grid.is_infected(self._position)
 
     def _move_forward(self):
         x, y = self._position
@@ -55,6 +55,40 @@ class Virus:
         self._position = (x, y)
 
 
+class Virus2(Virus):
+    def _burst(self):
+        if self._is_current_node_cleaned():
+            self._turn_left()
+            self._weaken()
+        elif self._is_current_node_weakened():
+            self._infect()
+        elif self._is_current_node_infected():
+            self._turn_right()
+            self._flag()
+        elif self._is_current_node_flagged():
+            self._reverse()
+            self._clean()
+        self._move_forward()
+
+    def _is_current_node_cleaned(self):
+        return self._grid.is_cleaned(self._position)
+
+    def _weaken(self):
+        self._grid.weaken(self._position)
+
+    def _is_current_node_weakened(self):
+        return self._grid.is_weakened(self._position)
+
+    def _flag(self):
+        self._grid.flag(self._position)
+
+    def _is_current_node_flagged(self):
+        return self._grid.is_flagged(self._position)
+
+    def _reverse(self):
+        self._direction_index = (self._direction_index + 2) % 4
+
+
 class Grid:
     def __init__(self, strings):
         self._dict = defaultdict(lambda: False)
@@ -62,7 +96,7 @@ class Grid:
             for x, character in enumerate(row):
                 self._dict[(x, y)] = (character == '#')
 
-    def infected(self, position):
+    def is_infected(self, position):
         return self._dict[position]
 
     def infect(self, position):
@@ -88,9 +122,60 @@ class Grid:
         return '\n'.join(output)
 
 
+class Grid2:
+    def __init__(self, strings):
+        self._dict = defaultdict(lambda: '.')
+        for y, row in enumerate(strings):
+            for x, character in enumerate(row):
+                self._dict[(x, y)] = character
+
+    def is_cleaned(self, position):
+        return self._dict[position] == '.'
+
+    def is_infected(self, position):
+        return self._dict[position] == '#'
+
+    def is_weakened(self, position):
+        return self._dict[position] == 'W'
+
+    def is_flagged(self, position):
+        return self._dict[position] == 'F'
+
+    def clean(self, position):
+        self._dict[position] = '.'
+
+    def infect(self, position):
+        self._dict[position] = '#'
+
+    def weaken(self, position):
+        self._dict[position] = 'W'
+
+    def flag(self, position):
+        self._dict[position] = 'F'
+
+    def __str__(self):
+        output = []
+        first_row = min(self._dict.keys(), key=lambda x: x[1])[1]
+        last_row = max(self._dict.keys(), key=lambda x: x[1])[1]
+        first_column = min(self._dict.keys(), key=lambda x: x[0])[0]
+        last_column = max(self._dict.keys(), key=lambda x: x[0])[0]
+        for y in range(first_row, last_row + 1):
+            current_string = ''
+            for x in range(first_column, last_column + 1):
+                current_string += self._dict[(x, y)] + ' '
+            output.append(current_string)
+        return '\n'.join(output)
+
+
 if __name__ == '__main__':
     grid = Grid(input_rows(22))
     virus = Virus(grid, (12, 12))
     for _ in range(10000):
         virus._burst()
     print(virus._infect_count)
+
+    grid2 = Grid2(input_rows(22))
+    virus2 = Virus2(grid2, (12, 12))  # Don't name classes this way, easy typo
+    for _ in range(10000000):
+        virus2._burst()
+    print(virus2._infect_count)
--- test_day22.py
+++ test_day22.py
@@ -1,8 +1,8 @@
-from day22 import Virus, Grid
+from day22 import Virus, Grid, Virus2, Grid2
 
 
 class TestVirus:
-    def test_is_current_node_infected(self):
+    def test_is_current_node_is_infected(self):
         grid = Grid('... .#. ...'.split())
         virus = Virus(grid, (1, 1))
         assert virus._is_current_node_infected()
@@ -50,20 +50,45 @@ class TestVirus:
         assert virus._infect_count == 5587
 
 
+class TestVirus2:
+    def test_example_short(self):
+        grid = Grid2('..# #.. ...'.split())
+        virus = Virus2(grid, (1, 1))
+        for _ in range(100):
+            virus._burst()
+        assert virus._infect_count == 26
+
+    def test_print(self):
+        grid = Grid2('..# #.. ...'.split())
+        virus = Virus2(grid, (1, 1))
+        for _ in range(8):
+            print(grid)
+            print()
+            virus._burst()
+
+            # Passes, but too slow.
+            # def test_example_long(self):
+            #     grid = Grid2('..# #.. ...'.split())
+            #     virus = Virus2(grid, (1, 1))
+            #     for _ in range(10000000):
+            #         virus._burst()
+            #     assert virus._infect_count == 2511944
+
+
 class TestGrid:
-    def test_infected(self):
+    def test_is_infected(self):
         grid = Grid('... .#. ...'.split())
-        assert grid.infected((1, 1))
-        assert not grid.infected((2, 2))
+        assert grid.is_infected((1, 1))
+        assert not grid.is_infected((2, 2))
 
     def test_infect(self):
         grid = Grid('... .#. ...'.split())
-        assert not grid.infected((2, 2))
+        assert not grid.is_infected((2, 2))
         grid.infect((2, 2))
-        assert grid.infected((2, 2))
+        assert grid.is_infected((2, 2))
 
     def test_clean(self):
         grid = Grid('... .#. ...'.split())
-        assert grid.infected((1, 1))
+        assert grid.is_infected((1, 1))
         grid.clean((1, 1))
-        assert not grid.infected((1, 1))
+        assert not grid.is_infected((1, 1))
